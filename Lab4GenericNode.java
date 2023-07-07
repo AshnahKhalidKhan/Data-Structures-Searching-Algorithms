5dcccc6b04307ffc01c103aa57caed49df0dce4d
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-07-07 23:48:47 +0500

    Add files via upload

---
+++ Lab4GenericNode.java
@@ -0,0 +1,13 @@
+//ASHNAH KHALID KHAN ERP # 22889
+
+public class Lab4GenericNode<Type>
+{
+	Type data;
+	Lab4GenericNode<Type> next;
+
+	Lab4GenericNode(Type DATA)
+	{
+		data = DATA;
+		next = null;
+	}
+}
