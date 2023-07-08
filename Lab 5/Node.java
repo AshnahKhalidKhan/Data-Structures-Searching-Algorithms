635ed8b3ac8639120db802c001b77640acb6c090
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-07-08 22:47:02 +0500

    Add files via upload

---
+++ Lab 5/Node.java
@@ -0,0 +1,15 @@
+//ASHNAH KHALID KHAN ERP # 22889
+
+public class Node
+{
+	int coefficient;
+	int power;
+	Node prev;
+	Node next;
+
+	Node(int c, int p)
+	{
+		coefficient = c;
+		power = p;
+	}
+}
