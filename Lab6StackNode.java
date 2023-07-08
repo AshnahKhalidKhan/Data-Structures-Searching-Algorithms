39ab87ebc3b8524fca01d9919a52cc039b052bb3
AshnahKhalidKhan <107252209+AshnahKhalidKhan@users.noreply.github.com>
2023-07-08 22:51:16 +0500

    Add files via upload

---
+++ Lab6StackNode.java
@@ -0,0 +1,13 @@
+//ASHNAH KHALID KHAN ERP # 22889
+
+public class Lab6StackNode<Type>
+{
+	Type info;
+	Lab6StackNode<Type> next;
+
+	public Lab6StackNode(Type data)
+	{
+		info = data;
+		next = null;
+	}
+}
